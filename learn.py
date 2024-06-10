fee96903cdc5d8ef20c5584583626c72f6da08a7
CrayCrayUltraContrarian <106885248+craycrayultracontrarian@users.noreply.github.com>
2024-06-10 14:11:17 -0400

    w

--- learn.py
+++ learn.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
