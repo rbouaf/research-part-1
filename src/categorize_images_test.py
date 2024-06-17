67b66905dfe5d2168de197f9c9c1ad0e61749a35
CrayCrayUltraContrarian <106885248+craycrayultracontrarian@users.noreply.github.com>
2024-06-17 16:08:30 -0400

    added error handling for hallucinations

--- src/categorize_images_test.py
+++ src/categorize_images_test.py
@@ -1,16 +1,12 @@
 from openai import OpenAI
-with open('topic_list.txt', 'r') as file:
-    topics = file.read()
-client = OpenAI(
-    api_key="sk-proj-UIx0DjExZFZ3K8zNgOZmT3BlbkFJj0cGIb1imfb4V3840rQY"
-)
-
-thumbnails = [
-      "https://instagram.fymq2-1.fna.fbcdn.net/v/t51.29350-15/447812387_835561961812626_7109181616597521791_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi43MjB4MTI4MC5zZHIuZjI5MzUwIn0&_nc_ht=instagram.fymq2-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=sC_BLUYAI04Q7kNvgGzGmZK&edm=APb0JzIBAAAA&ccb=7-5&ig_cache_key=MzM4Mzg5NjkyNDU0ODM1MDUwOA%3D%3D.2-ccb7-5&oh=00_AYBx28ZmMlyzc2yLx8lNLM0dGpjH6AEoqGzqVmIxPXAElg&oe=66763EA8&_nc_sid=cf751b",
-      "https://instagram.fymq2-1.fna.fbcdn.net/v/t51.29350-15/446100134_827185172728333_6074243709158091986_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi43MjB4MTI4MC5zZHIuZjI5MzUwIn0&_nc_ht=instagram.fymq2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=_WiIWMopUGwQ7kNvgF309E8&edm=APb0JzIBAAAA&ccb=7-5&ig_cache_key=MzM3NTg1MTI4OTE5ODYzOTgzOQ%3D%3D.2-ccb7-5&oh=00_AYCuGEeWFqmEnfsLviTjr2ZV6KmwyNlU9kvk6KffFT-dBw&oe=667657FD&_nc_sid=cf751b"
-       ]
-n=0
+from file_list_topics import list_topics
+with open('topic_list.txt', 'r') as file: topics = file.read()
+client = OpenAI(api_key="sk-proj-UIx0DjExZFZ3K8zNgOZmT3BlbkFJj0cGIb1imfb4V3840rQY")
+thumbnails = ["https://instagram.fymq2-1.fna.fbcdn.net/v/t51.29350-15/447812387_835561961812626_7109181616597521791_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi43MjB4MTI4MC5zZHIuZjI5MzUwIn0&_nc_ht=instagram.fymq2-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=sC_BLUYAI04Q7kNvgGzGmZK&edm=APb0JzIBAAAA&ccb=7-5&ig_cache_key=MzM4Mzg5NjkyNDU0ODM1MDUwOA%3D%3D.2-ccb7-5&oh=00_AYBx28ZmMlyzc2yLx8lNLM0dGpjH6AEoqGzqVmIxPXAElg&oe=66763EA8&_nc_sid=cf751b","https://instagram.fymq2-1.fna.fbcdn.net/v/t51.29350-15/446100134_827185172728333_6074243709158091986_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi43MjB4MTI4MC5zZHIuZjI5MzUwIn0&_nc_ht=instagram.fymq2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=_WiIWMopUGwQ7kNvgF309E8&edm=APb0JzIBAAAA&ccb=7-5&ig_cache_key=MzM3NTg1MTI4OTE5ODYzOTgzOQ%3D%3D.2-ccb7-5&oh=00_AYCuGEeWFqmEnfsLviTjr2ZV6KmwyNlU9kvk6KffFT-dBw&oe=667657FD&_nc_sid=cf751b"]
 posts = []
+errors = []
+n=0
+
 while n < len(thumbnails) :
   completion = client.chat.completions.create(
     model="gpt-4o",
@@ -25,7 +21,7 @@ while n < len(thumbnails) :
           "content": [
             {
               "type": "text",
-              "text": "WHAT IS IN THE IMAGE? DESCRIBE IT USING 3 TOPICS FROM THE AFORMENTIONED LIST, IN THIS FORMAT: topic1, topic2, topic3."
+              "text": "WHAT IS IN THE IMAGE? DESCRIBE IT USING 3 TOPICS FROM THE AFORMENTIONED LIST, IN THIS FORMAT: topic1, topic2, topic3"
             },
             {
               "type": "image_url",
@@ -40,15 +36,16 @@ while n < len(thumbnails) :
         }
       ]
   )
+  cur_post = completion.choices[0].message.content.split(", ")
+  for index, value in enumerate(cur_post):
+    if value not in list_topics:
+        errors.append(f"Invalid topic at entry {n}, {index}, contained {value} from {cur_post}")
+
   posts.append(completion.choices[0].message.content) 
   n+=1
 
-print(posts) 
-  
-
-  # make a 'check' function that verifies that the 3 categories
-  # are in the list of topics and that they are unique (avoid hallucinations)
-
+print(posts)
+print(errors)  
 
   # run this code concurrently with the other code that scrolls reels? 
 
