ad2fbb400693882f8b8fc9fbaf55165837459c62
CrayCrayUltraContrarian <106885248+craycrayultracontrarian@users.noreply.github.com>
2024-06-26 13:02:17 -0400

    cleanup + AWS file upload integrated to selenium

--- src/bastard.py
+++ src/bastard.py
@@ -6,9 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 import time
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 from data.keys.credentials import aws_access_key_id, aws_secret_access_key
 
